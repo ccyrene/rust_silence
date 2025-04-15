13224002e742bc6fb5d4d4822a570c4ed6f68222
ccyrene <rungrodks@hotmail.com>
2025-04-15 13:43:10 +0700

    add core module

--- rust_silence/__init__.py
+++ rust_silence/__init__.py
@@ -1,4 +1,11 @@
-from .core import from_file
-from .core import split_on_silence
+from .core import (
+    from_file,
+    detect_silence,
+    detect_nonsilent,
+    split_on_silence,
+    detect_leading_silence,
+    remove_silence_edges,
+    preprocess_f5
+)
 
 __version__ = "0.1.0"
--- rust_silence/core.py
+++ rust_silence/core.py
@@ -7,6 +7,38 @@ from typing import Tuple, List
 def from_file(file: bytes) -> Tuple[npt.NDArray[np.float32], int]:
     return _rust_silence.audio_bytes_to_f32_samples_py(file)
 
+def detect_silence(
+    samples: npt:NDArray[np.float32],
+    sample_rate: int,
+    min_silence_len_ms: int = 1000,
+    silence_thresh_db: float = -16.0,
+    seek_step_ms: int = 1
+) -> List[[int, int]]:
+    
+    return _rust_silence.detect_silence_py(
+        samples,
+        sample_rate,
+        min_silence_len_ms,
+        silence_thresh_db,
+        seek_step_ms
+    )
+
+def detect_nonsilent(
+    samples: npt.NDArray[np.float32],
+    sample_rate: int,
+    min_silence_len_ms: int = 1000,
+    silence_thresh_db: float = -16.0,
+    seek_step_ms: int = 1
+) -> List[[int, int]]:
+    
+    return _rust_silence.detect_nonsilent_py(
+        samples,
+        sample_rate,
+        min_silence_len_ms,
+        silence_thresh_db,
+        seek_step_ms
+    )
+
 def split_on_silence(
     samples: npt.NDArray[np.float32],
     sample_rate: int,
@@ -23,4 +55,48 @@ def split_on_silence(
         silence_thresh_db,
         keep_silence_ms,
         seek_step_ms
+    )
+    
+def detect_leading_silence(
+    samples: npt:NDArray[np.float32],
+    sample_rate: int,
+    silence_thresh_db: float = -50.0,
+    chunk_size_ms: int = 10
+) -> int:
+    
+    return _rust_silence.detect_leading_silence_py(
+        samples,
+        sample_rate,
+        silence_thresh_db,
+        chunk_size_ms
+    )
+    
+def remove_silence_edges(
+    samples: npt:NDArray[np.float32],
+    sample_rate: int,
+    silence_thresh_db: float = -42.0,
+    chunk_size_ms: int = 10
+) -> npt.NDArray[np.float32]:
+    
+    return _rust_silence.remove_silence_edges_py(
+        samples,
+        sample_rate,
+        silence_thresh_db,
+        chunk_size_ms
+    )
+    
+def preprocess_f5(
+    samples: npt.NDArray[np.float32],
+    sample_rate: int,
+    silence_thresh_db: float = -42.0,
+    chunk_size_ms: int = 10,
+    clip_short: bool = True
+) -> npt.NDArray[np.float32]:
+    
+    return _rust_silence.preprocess_f5_py(
+        samples,
+        sample_rate,
+        silence_thresh_db,
+        chunk_size_ms,
+        clip_short
     )
